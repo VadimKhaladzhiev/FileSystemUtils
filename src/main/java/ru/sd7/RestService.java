0a6dd663744fbfa5d2729fc9f41143567c81a462
Vadim Khaladzhiev <khaladzhiev@tbm.ru>
2016-10-26 15:48:55 +0300

    Log4j added

--- src/main/java/ru/sd7/RestService.java
+++ src/main/java/ru/sd7/RestService.java
@@ -1,24 +1,34 @@
 package ru.sd7;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Configuration
+@ImportResource("classpath:applicaitonContext.xml")
 @RestController
 @EnableAutoConfiguration
 public class RestService {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping("/list")
     List<SearchResult> list(@RequestParam(value="dir", defaultValue="src") String name,
                             @RequestParam(value="keyword", defaultValue="class") String keyword) {
+        logger.info("List dir:{} keyword:{}", name, keyword);
         return new DirSearcher().search(new SearchParams(name, keyword));
     }
 
     @RequestMapping("/count")
     int count(@RequestParam(value="dir", defaultValue="src") String name,
                             @RequestParam(value="keyword", defaultValue="class") String keyword) {
+        logger.info("List dir:{} keyword:{}", name, keyword);
         List<SearchResult> list = new DirSearcher().search(new SearchParams(name, keyword));
         return list.size();
     }
