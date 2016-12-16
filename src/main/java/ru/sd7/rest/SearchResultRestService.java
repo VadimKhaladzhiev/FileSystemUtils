b01964318c3448efead0cb75e8588b4729948308
Vadim Khaladzhiev <khaladzhiev@tbm.ru>
2016-12-16 09:46:56 +0300

    SearchResults REST controller

---
+++ src/main/java/ru/sd7/rest/SearchResultRestService.java
@@ -0,0 +1,43 @@
+package ru.sd7.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.sd7.model.SearchResult;
+import ru.sd7.services.spring.api.SearchResultService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/search_results")
+public class SearchResultRestService {
+
+    @Autowired
+    private SearchResultService searchResultService;
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public List<SearchResult> showAll() {
+        return searchResultService.getAll();
+    }
+
+    @RequestMapping(value = "/by_id", method = RequestMethod.GET)
+    public SearchResult getById(@RequestParam(required = true) Long id) {
+        return searchResultService.get(id);
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public boolean add(SearchResult item) {
+        if(item.getId() == null) searchResultService.add(item);
+        else searchResultService.update(item);
+
+        return true;
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.GET)
+    public boolean deleteContact(@RequestParam(required = true) Long id) {
+        searchResultService.remove(id);
+        return true;
+    }
+}
